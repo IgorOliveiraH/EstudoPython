ee37c47465268dfab14ab790eaca6eac4ea3bce2
IgorOliveiraH <igor.oliveirasouzasilva@gmail.com>
2025-12-14 16:17:14 -0300

    Adicionando exercicio da aula 02

---
+++ SRC/aula02.py
@@ -0,0 +1,33 @@
+""""aula 02 - keywords e identificadores"""
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# nao pode ter espaços em branco
+# nao pode usar caracteres especias !, @, #, $
+
+nome = 'Maria'
+idade = 30
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+# Boas práticas (clean code)
+c = 10
+contator = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+#constantes
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+if idade >=MAIORIDADE:
+    print('Maior de idade')
+else:
+    print("menor de idade")
