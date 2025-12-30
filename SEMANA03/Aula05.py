73ecb40a549b25e04cf46507c542a7799cc5d680
IgorOliveiraH <igor.oliveirasouzasilva@gmail.com>
2025-12-30 15:54:01 -0300

    Adicionando debug aula05

---
+++ SEMANA03/Aula05.py
@@ -0,0 +1,17 @@
+''' Aula 01 - debug'''
+
+def somar(nota1,nota2,nota3):
+    soma = nota1+nota2+nota3
+    return soma
+
+def calcular_media(nota1,nota2,nota3):
+    soma = somar(nota1,nota2,nota3)
+    media = soma/3
+    return media
+
+nota1 = 10.0
+nota2 = 3.0
+nota3 = 5.5
+
+media = calcular_media(nota1,nota2,nota3)
+print (media)
